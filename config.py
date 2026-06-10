5b8f6541a3d9973630adca8cf4d731ddee35da7f
skuaej <sumankumar821310@gmail.com>
2026-06-10 12:00:14 +0530

    Modify default values for configuration settings
    Updated environment variable defaults for queue, duration, playlist limits, and YouTube API key.

--- config.py
+++ config.py
@@ -26,10 +26,10 @@ class Config:
         self.AUTO_LEAVE: bool = getenv("AUTO_LEAVE", False)
         self.VIDEO_PLAY: bool = getenv("VIDEO_PLAY", True)
 
-        self.QUEUE_LIMIT = int(getenv("QUEUE_LIMIT", "50"))
-        self.DURATION_LIMIT = int(getenv("DURATION_LIMIT", "5400"))
-        self.PLAYLIST_LIMIT = int(getenv("PLAYLIST_LIMIT", "20"))
-        self.YOUTUBE_API_KEY = getenv("YOUTUBE_API_KEY", "INFLEX68575028D")
+        self.QUEUE_LIMIT = int(getenv("QUEUE_LIMIT", "8"))
+        self.DURATION_LIMIT = int(getenv("DURATION_LIMIT", "100"))
+        self.PLAYLIST_LIMIT = int(getenv("PLAYLIST_LIMIT", "10"))
+        self.YOUTUBE_API_KEY = getenv("YOUTUBE_API_KEY", "INF28D")
         self.COOKIES_URL = [
             url for url in getenv("COOKIES_URL", "").split(" ")
             if url and "batbin.me" in url
